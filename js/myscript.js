7344b6c77fb758a09a4b0be7122d10a2eb656a43
alpal309 <34352035+alpal309@users.noreply.github.com>
2019-10-31 21:49:40 -0500

    Add files via upload

--- js/myscript.js
+++ js/myscript.js
@@ -1,5 +1,6 @@
 document.getElementsByClassName("close")[0].addEventListener("click", closeit);
 document.getElementsByClassName("mobileMenuTrigger")[0].addEventListener("click", mobileMenuFunc);
+window.onload = slideshow();
 
 var exit = document.getElementsByClassName("exit");
 for(var j = 0; j < exit.length; j++) {
@@ -30,4 +31,24 @@ function closeit() {
 	document.getElementsByClassName("mobileMenuTrigger")[0].style.display = "inline-block";
 	document.body.style.overflow = "visible";
 	document.body.style.padding = "0 5%";
+}
+
+function slideshow(){
+	var slide = document.getElementsByClassName("imgdivsection");
+	var slideIndex = 0;
+	slide[slideIndex].style.display = "block";
+
+	setInterval(function(){
+
+		if(slideIndex >= slide.length)
+			slideIndex = 0;
+
+		if(slideIndex != 0)
+			slide[slideIndex-1].style.display = "none";
+
+		slide[slideIndex].style.display = "block";
+		slideIndex++;
+
+	}, 5000);
+
 }
